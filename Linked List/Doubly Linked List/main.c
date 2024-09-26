b429d4c6a7303a7b41c68446ba6056da1e7209d0
Faisal786111 <faisalkhanisrar@gmail.com>
2024-09-26 17:38:43 +0530

    changes in doubly linked list

--- Linked List/Doubly Linked List/main.c
+++ Linked List/Doubly Linked List/main.c
@@ -1,12 +1,43 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "operations.h"
 
-int main(){
-    create();
-    traverse();
+int main() {
+    int choice;
 
-    // insertBefore();
-    delete();
-    traverse();
+    while (1) {
+        printf("\n--- Circular Linked List Menu ---\n");
+        printf("1. Create Circular Linked List\n");
+        printf("2. Traverse Circular Linked List\n");
+        printf("3. Insert Before\n");
+        printf("4. Insert After\n");
+        printf("5. Delete Element\n");
+        printf("6. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                create();
+                break;
+            case 2:
+                traverse();
+                break;
+            case 3:
+                insertBefore();
+                break;
+            case 4:
+                insertAfter();
+                break;
+            case 5:
+                delete();
+                break;
+            case 6:
+                printf("Exiting...\n");
+                return 0;
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+    }
+
     return 0;
 }
