d98e8e84949b83d10d2cfca7416b3898adbd2272
Faisal786111 <faisalkhanisrar@gmail.com>
2024-12-04 19:02:17 +0530

    changes in Linear Probing

--- Hash/Linear Probing/main.c
+++ Hash/Linear Probing/main.c
@@ -1,36 +1,76 @@
+
 #include <stdio.h>
 #define SIZE 10
 
 int hash(int key){
-    return key % SIZE;
+    return key % 10;
 }
 
-int probe(int H[] , int key){
+int prob(int HT[] , int key){
     int index = hash(key);
-    int i = 0; 
-    while(H[(index + i)%SIZE]!=0){
+    
+    int i = 0 ; 
+    while(HT[(index + i)  % SIZE] != 0){
         i++;
     }
-    return (index + i) %SIZE;
+    
+    return (index + i) % SIZE;
 }
 
-void Insert(int H[],int key){
+void Insert(int HT[] ,int key){
     int index = hash(key);
-
+    
     // already occupied
-    if(H[index] != 0){
-        index = probe(H , key);        
+    if(HT[index] != 0){
+        index = prob(HT , key);
     }
-    H[index] = key;
+    
+    HT[index]=key;
+}
+
+int Search(int HT[]  , int key){
+    int index = hash(key);
+    
+    int i = 0;
+     // Linear probing to find the key
+    while(HT[(index + i) % SIZE] != 0){
+        if(HT[(index + i) % SIZE] == key){
+            return (index + i) % SIZE; // Return the index if found
+        }
+        i++;
+    }
+    
+    return -1; // Return -1 if the key is not found
+}
+
+void display(int HT[]){
+    int i;
+    for( i = 0 ; i < SIZE ; i++){
+        // only occupied elements will be displayed 
+        if(HT[i] != 0 ){
+            printf(" %d " , HT[i]);
+        }
+    }
 }
 
 
-int main(){
-    int HT[SIZE] = {0};
+int main()
+{
+    int HT[10] = {0};
 
-    Insert(HT , 22);
-    Insert(HT , 42);
-    Insert(HT , 33);
+    Insert(HT , 20);
+    Insert(HT , 23);
+    Insert(HT , 40);
+    Insert(HT , 25);
+    Insert(HT , 30);
+    
+    display(HT);
+    
+      // Searching for keys
+    printf("\nSearch 20: %d\n", Search(HT, 20)); // Should return the index where 20 is stored
+    printf("Search 25: %d\n", Search(HT, 25)); // Should return the index where 25 is stored
+    printf("Search 50: %d\n", Search(HT, 50)); // Should return -1 (not found)
+
 
     return 0;
 }
