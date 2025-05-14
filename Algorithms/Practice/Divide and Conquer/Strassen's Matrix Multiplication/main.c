fd2a01d5891c472fe4679e92a2cca51afa6058d9
Faisal786111 <faisalkhanisrar@gmail.com>
2025-05-14 11:24:14 +0530

    Changes in SMM

--- Algorithms/Practice/Divide and Conquer/Strassen's Matrix Multiplication/main.c
+++ Algorithms/Practice/Divide and Conquer/Strassen's Matrix Multiplication/main.c
@@ -1,57 +1,60 @@
 #include <stdio.h>
 
+#define d 3
 
 
-int main(){
-    int A[2][2], B[2][2], C[2][2];
-    int i, j, P, Q, R, S, T, U, V;
-
-    printf("Enter A matrix: \n");
-    for(i = 0; i< 2; i++){
-        for(j = 0; j <2; j++)
+int main()
+{
+    int n = 2;
+    int A[d][d], B[d][d], C[d][d];
+    int P, Q, R, S, T, U, V;
+    int i, j;
+    
+    printf("Enter A matrix: ");
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= n; j++)
             scanf("%d", &A[i][j]);
-    }  
-
-    printf("Enter B matrix: \n");
-    for(i = 0; i< 2; i++){
-        for(j = 0; j <2; j++)
+    }
+    
+    printf("Enter B matrix: ");
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= n; j++)
             scanf("%d", &B[i][j]);
-    } 
-
-    printf("\nA matrix: \n");
-    for(i = 0; i< 2; i++){
-        for(j = 0; j <2; j++)
-            printf("%d\t", A[i][j]);
+    }
+    
+    printf("\nA Matrix: \n");
+    for(i = 1; i <= n; i++)
+        for(j = 1; j <= n; j++)
+            printf(" %d ", A[i][j]);
         printf("\n");
-    }   
-    printf("\nB matrix: \n");
-    for(i = 0; i< 2; i++){
-        for(j = 0; j <2; j++)
-            printf("%d\t", B[i][j]);
+    
+    printf("\nB Matrix: \n");
+    for(i = 1; i <= n; i++)
+        for(j = 1; j <= n; j++)
+            printf(" %d ", A[i][j]);
         printf("\n");
-    }   
-
-    // SMM 
-    P = (A[0][0] + A[1][1]) * (B[0][0] + B[1][1]);
-    Q = (A[1][0] + A[1][1]) * B[0][0];
-    R = A[0][0] * (B[0][1] - B[1][1]);
-    S = A[1][1] * (B[1][0] - B[0][0]);
-    T = (A[0][0] + A[0][1]) * B[1][1];
-    U = (A[1][0] - A[0][0]) * (B[0][0] + B[0][1]);
-    V = (A[0][1] - A[1][1]) * (B[1][0] + B[1][1]);
-
-    // Calculate result matrix 
-    C[0][0] = P + S - T + V;
-    C[0][1] = R + T;
-    C[1][0] = Q + S;
-    C[1][1] = P - Q + R + U;
-
-    printf("\nC matrix: \n");
-    for(i = 0; i< 2; i++){
-        for(j = 0; j <2; j++)
-            printf("%d\t", C[i][j]);
+        
+    // SMM Working 
+    P = (A[1][1] + A[2][2]) * (B[1][1] + B[2][2]);
+    Q = (A[2][1] + A[2][2]) * B[1][1];
+    R = A[1][1] * (B[1][2] - B[2][2]); 
+    S = A[2][2] * (B[2][1] - B[1][1]);
+    T = (A[1][1] + A[1][2]) * B[2][2];
+    U = (A[2][1] - A[1][1]) * (B[1][1] + B[1][2]); 
+    V = (A[1][2] - A[2][2]) * (B[2][1] + B[2][2]); 
+    
+    // Calculate Result matrix 
+    C[1][1] = P + S - T + V;
+    C[1][2] = R + T;
+    C[2][1] = Q + S;
+    C[2][2] = P - Q + R + U;
+    
+    // Result matrix 
+    printf("\nC Matrix: \n");
+    for(i = 1; i <= n; i++)
+        for(j = 1; j <= n; j++)
+            printf(" %d ", C[i][j]);
         printf("\n");
-    } 
-
+    
     return 0;
 }
