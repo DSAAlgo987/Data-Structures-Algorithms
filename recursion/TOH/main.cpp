bfeba94df5fe5dd2ef480d8f2492247ad9aff566
Faisal786111 <faisalkhanisrar@gmail.com>
2025-06-04 23:42:20 +0530

    TOH completed

---
+++ recursion/TOH/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int s, int d, int h, int n){
+    // BC 
+    if(n == 1){
+        cout << "Move "<< n << " plate from "<< s << " " << d << endl;
+        return;
+    }
+}
+
+int main(){
+    int n = 3;
+    int s = 1;
+    int h = 2; 
+    int d = 3;
+
+    solve(s, h, d, n);
+
+    return 0;
+}
