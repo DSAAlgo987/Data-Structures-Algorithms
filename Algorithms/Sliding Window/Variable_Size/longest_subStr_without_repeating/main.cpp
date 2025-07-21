6e88cb3d072bd9a831a8f03d240d643f1af0aca0
Faisal Khan <faisal@example.com>
2025-07-21 23:29:36 +0530

    Longest substring without repeating chars

---
+++ Algorithms/Sliding Window/Variable_Size/longest_subStr_without_repeating/main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem: Longest substring with all unique characters 
+
+int longestSubstringWithoutRepeating(string &s){
+    int i = 0, j = 0, mx = INT_MIN; 
+    unordered_map<char, int> mp;
+
+    while(j < s.size()){
+        // Calculation
+        mp[s[j]]++;
+
+        // mp size same as window size
+        if(mp.size() == j - i + 1){
+            // Ans <- calculation 
+            mx = max(mx, j - i + 1);
+            j++;
+        }
+        // mp size less than window size
+        else if(mp.size() < j - i + 1){
+            // Remove ith element until equal or greater than window size 
+            while(mp.size() < j - i + 1){
+                // Remove ith calculation 
+                mp[s[i]]--;
+
+                if(mp[s[i]] == 0){
+                    mp.erase(s[i]);
+                }
+
+                i++;
+            }
+            j++;
+        }
+    }
+
+    return mx;
+}
+
+int main(){
+    string s = "geeksforgeeks";
+
+    int ans = longestSubstringWithoutRepeating(s);
+
+    cout << ans << endl;
+    return 0;
+}
