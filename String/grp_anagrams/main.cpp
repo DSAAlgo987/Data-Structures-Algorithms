313dd9d363e1bc270bb768910c76e3fd10a2926a
Faisal Khan <faisal@example.com>
2025-07-23 17:19:34 +0530

    group_anagrams

---
+++ String/grp_anagrams/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// nlgn
+vector<vector<string>> groupAnagrams(vector<string> str){
+    unordered_map<string, vector<string>> mp;
+
+    for(auto &s: str){
+        string sorted_str = s;
+        sort(sorted_str.begin(), sorted_str.end());
+
+        mp[sorted_str].push_back(s);
+    }
+
+    vector<vector<string>> result;
+
+    for(auto it: mp){
+        result.push_back(it.second);
+    }
+
+    return result;
+}
+
+int main(){
+    vector<string> str = {"tea", "aet", "eat", "bat", "nat", "tan"};
+
+    vector<vector<string>> ans = groupAnagrams(str);
+
+    for(auto i: ans){
+        for(auto s: i){
+            cout << s << " ";
+        }
+    }
+    return 0;
+}
