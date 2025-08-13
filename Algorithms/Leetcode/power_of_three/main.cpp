6ea36a2e3ac866946f37aab374fc082f143a06fb
Faisal Khan <faisal@example.com>
2025-08-13 20:58:45 +0530

    Completed power of three with all four apporaches

---
+++ Algorithms/Leetcode/power_of_three/main.cpp
@@ -0,0 +1,58 @@
+// Recursion based: log(N)
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        // Base conditions 
+        if(n <= 0){ // Powers are only in positive  
+            return false;
+        }
+        else if(n == 1){
+            return true;
+        }else if(n % 3 != 0){
+            return false;
+        } 
+        return isPowerOfThree(n/3);
+    }
+};
+
+// Loop based: log(N)
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        // Negative 
+        if(n <= 0){
+            return false;
+        }
+
+        while(n % 3 == 0){
+            n /= 3;
+        }
+
+        return n == 1;
+    }
+};
+
+// Log based: O(1) : This will satisfy all the test cases.
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        double x = log(n) / log(3);
+        return n > 0 && x == (int) x;
+    }
+};
+
+// Math based: O(1) 
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        // Negative number 
+        if(n <= 0){
+            return false;
+        }
+
+        
+        // 3^p % n == 0 -> (yes) 1 (no)
+        // 3^19 = 1162261467
+        return 1162261467 % n == 0;
+    }
+};
