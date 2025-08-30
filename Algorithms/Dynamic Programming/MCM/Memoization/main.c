77b95c8c76adb8972b1af5ef861c4d7c2b25bbb2
Faisal <124188437+Faisal786111@users.noreply.github.com>
2025-08-30 22:14:47 +0000

    completed memoized approach of MCM

--- Algorithms/Dynamic Programming/MCM/Memoization/main.c
+++ Algorithms/Dynamic Programming/MCM/Memoization/main.c
@@ -1,67 +1,46 @@
-#include <stdio.h>
-#include <limits.h>
-#include <string.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-/**
- *  n <= 1000 
- */
-int dp[1001][1001];
-int bracket[1001][1001]; // to store split point
+/*
+    Problem Statement: Matrix Chain Multiplication
+    Memoize(Top down) Approach: 
+    Time complexity: O(2^n)
+*/
 
-int solve(int arr[], int i, int j){
-    // Base condition
-    if(i >= j){
+vector<vector<int>> t; // Global declaration 
+
+int solve(vector<int> &A, int i, int j) {
+    // Base condition 
+    if(i >= j) {
         return 0;
     }
     
-    // Presnet or not 
-    if(dp[i][j] != -1){
-        return dp[i][j];
+    // Present in t ?
+    if(t[i][j] != -1) {
+        return t[i][j];
     }
     
-    int min = INT_MAX;
-    // k loop
-    for(int k = i; k <= j-1; k++){
-        // temp ans 
-        int tempAns = solve(arr, i, k) + solve(arr, k+1, j) + arr[i-1] * arr[k] * arr[j];
+    int mn = INT_MAX;
+    
+    // find k loop scheme 
+    for(int k = i; k <= j-1; k++) {
+        // Calc tempAns
+        int tempAns = solve(A, i, k) + solve(A, k+1, j) + A[i-1] * A[k] * A[j];
         
-        // Idenitfy min cost from temp ans 
-        if(tempAns < min){
-            min = tempAns;
-            bracket[i][j]= k;
-        }
+        // Calc ans from tempAns 
+        mn = min(mn, tempAns);
     }
     
-    return dp[i][j] = min;
+    return t[i][j] = mn;
 }
 
-void printParenthesis(int i, int j, char *name){
-    // base condition 
-    if(i == j){
-        printf("%c", (*name)++);
-        return;
-    }
-    printf("(");
-    printParenthesis(i, bracket[i][j], name);
-    printParenthesis(bracket[i][j] + 1, j, name);
-    printf(")");
-}
-
-int main()
-{
-    int arr[] = {40, 20, 30, 10, 30};
+int main() {
+    vector<int> arr = {40, 20, 30, 10, 30};
+    int n = arr.size();
     
-    int n = sizeof(arr) / sizeof(arr[0]);
+    t.assign(n + 1, vector<int>(n + 1, -1)); // Initialization as -1 
     
-    // Initialize dp as -1 
-    memset(dp, -1, sizeof(dp));
-    printf("Min Cost: %d", solve(arr, 1, n-1));
-
-    char name = 'A';
-    printf("Optimal Parenthesization: \n");
-    printParenthesis(1, n - 1, &name);
-    printf("\n");
-
-
+    cout << solve(arr, 1, n - 1) << endl; // op : 26000
+    
     return 0;
 }
