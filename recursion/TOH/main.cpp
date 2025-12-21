78579b53e8bc82d9de1d3bf11ec2be79fa4e3f7c
Faisal Khan <faisal@example.com>
2025-12-21 14:00:03 +0530

    Changes in Recursion: TOH

--- recursion/TOH/main.cpp
+++ recursion/TOH/main.cpp
@@ -2,19 +2,21 @@
 using namespace std;
 
 void solve(int s, int d, int h, int n){
-    // BC 
-    if(n == 1){
-        cout << "Move "<< n << " plate from "<< s << " " << d << endl;
+    // Base condition 
+    if(n == 1) {
+        cout << "Move a Plate from " << s << " -> " << d << endl;
         return;
     }
-
-    // Smaller input 
-    solve(s, h, d, n - 1);
-
-    cout << "Move "<< n << " plate from "<< s << " " << d << endl;
-
+    
     // Induction 
-    solve(h, d, s, n-1);
+    // Step1: Move plates from src to helper (N-1) with the help of dest
+    solve(s, h, d, n - 1);
+    
+    // Step2: Move last plate from src to destition 
+    cout << "Move a Plate from " << s << " -> " << d << endl;
+    
+    // Step3: Move all the plates (n-1) from helper to the destination with help of the src 
+    solve(h, d, s, n - 1);
 }
 
 int main(){
@@ -23,6 +25,7 @@ int main(){
     int h = 2; 
     int d = 3;
 
+    // Hypothesis
     solve(s, h, d, n);
 
     return 0;
