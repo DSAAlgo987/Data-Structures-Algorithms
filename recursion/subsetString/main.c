a3a157fd2dd3cd8826b711b9a2ce5df155b267e5
Faisal Khan <faisal@example.com>
2025-12-21 15:52:44 +0530

    Changes in Recursion: Print Unique Substrings

--- recursion/subsetString/main.c
+++ recursion/subsetString/main.c
@@ -1,32 +1,50 @@
+#include <bits/stdc++.h>
 #include <stdio.h>
-#include <string.h>
 
-// Time complexity : O(2 ^ n)
-void subset(char *str, int index, char *substrs, int currentLen){
+using namespace std; 
 
-    // Base case 
-    if(str[index] == '\0'){
-        substrs[currentLen] = '\0';
-        printf("%s\n", substrs);
-        
+/* 
+    Problem: Print Unique Subsets
+    Time complexity: O(2^n)
+    Space complexity: O(n. 2^n)
+*/
+
+// ALiases 
+using st = unordered_set<string>;
+
+st subsets; 
+
+void displaySet(st &s) {
+    for(auto c: s) {
+        cout << c << " ";
+    }
+    
+    cout << endl;
+}
+
+// PrintUniqueSubsets: Ip/Op Method 
+void PrintUniqueSubsets(string ip, string op, int idx) {
+    // base condition 
+    if(idx == ip.size()) {
+        subsets.insert(op);
         return;
     }
-
-    substrs[currentLen] = str[index];
-        
-    // Include current char
-    subset(str, index+1, substrs, currentLen+1);
     
-    // Exclude current char 
-    subset(str, index+1, substrs, currentLen);
+    // Include 
+    PrintUniqueSubsets(ip, op + ip[idx], idx+1);
     
-    
+    // Exclude 
+    PrintUniqueSubsets(ip, op, idx+1);
 }
+
 
 int main()
 {
-    char substrs[100];
-    subset("ab", 0, substrs, 0);
+    string ip = "aab";
+    string op = "";
+    PrintUniqueSubsets(ip, op, 0);
+    
+    displaySet(subsets);
 
     return 0;
 }
