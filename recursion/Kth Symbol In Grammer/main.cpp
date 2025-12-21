9a668ccaea57ba25e5eaffc95867754793ef965a
Faisal Khan <faisal@example.com>
2025-12-21 13:29:36 +0530

    Changes in Recursion: KTh Symbol Grammar

--- recursion/Kth Symbol In Grammer/main.cpp
+++ recursion/Kth Symbol In Grammer/main.cpp
@@ -1,27 +1,40 @@
 #include <bits/stdc++.h>
-using namespace std;
+
+using namespace std; 
+
+/* 
+    Problem: Kth Symbol in Grammar 
+    IP: N(col) and K(row) 1 indexed based 
+    OP: Return 0/1
+    Time Complexity: O(N)
+    Space Complexity: O(N)
+*/
 
 
-int solve(int N, int k){
+int kThSymbol(int N, int K) {
     // Base condition 
-    if(N == 1 && k == 1){
-        return 0;
+    if(N == 1 && K == 1) {
+        return 0; 
     }
-
-    // Induction 
-    int mid = pow(2, N - 1);
-    if(k <= mid){
-        return solve(N - 1, k);
-    }else{
-        return !solve(N - 1, k - mid);
+    
+    // Induction: Make Ip Smaller wisely 
+    
+    // Identify the mid point 
+    int mid = pow(2, N-1) / 2; 
+    
+    // Check where k's lies 
+    if(K <= mid) {
+        // Normal Region
+        return kThSymbol(N-1, K);
+    } else {
+        // Complement Region
+        return !kThSymbol(N-1, K-mid);
     }
 }
 
-int main(){
-    
-    // Hypothesis
-    int ans = solve(4, 5);
-    cout << ans << endl;
+int main()
+{
+    cout << kThSymbol(3, 3) << endl;
 
     return 0;
 }
