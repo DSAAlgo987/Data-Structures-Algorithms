3f7344d162242f1c5983652e1c28e365536872db
Faisal Khan <faisal@example.com>
2026-01-19 09:32:20 +0530

    completed memoized lcs

--- Algorithms/Dynamic Programming/LCS/Memoization/main.cpp
+++ Algorithms/Dynamic Programming/LCS/Memoization/main.cpp
@@ -2,39 +2,68 @@
 using namespace std;
 
 /**
- * Problem Statement: Longest common subsequence 
- * 
- * Ip:
- * string x, y;
- * 
- * Op:
- * Display Length of LCS 
+ * PS: LCS (Longest Common Subsequence)
+ *
+ * Problem:
+ *   Given two strings x and y, find the length of the
+ *   Longest Common Subsequence (LCS) between them.
+ *   A subsequence maintains relative order but
+ *   characters need not be contiguous.
+ *
+ * IP:
+ *   - string x
+ *   - string y
+ *
+ * OP:
+ *   - int → length of the Longest Common Subsequence
+ *
+ * APPROACH:
+ *   - Top Down Dynamic Programming (Memoization)
+ *   - Recursion + DP table to store overlapping subproblems
+ *
+ * DP STATE:
+ *   - t[i][j] → length of LCS between
+ *               x[0..i-1] and y[0..j-1]
+ *
+ * TC:
+ *   - O(n × m)
+ *     Each (n, m) state is computed once due to memoization
+ *
+ * SC:
+ *   - O(n × m) → DP table
+ *   - O(n + m) → recursion stack (worst case)
+ *
+ * LEARNING:
+ *   - How recursion leads to overlapping subproblems
+ *   - Why memoization reduces exponential time to polynomial
+ *   - Clear identification of DP state and transitions
+ *   - Difference between brute-force LCS and optimized DP LCS
  */
 
-vector<vector<int>> t; // Dp Declaration 
+// Aliases: 
+using v = vector<int>;
+using vv = vector<v>;
 
-// Memoized approach : O(n * m)
-int LCS(string x, string y, int n, int m){
-    // Base condition 
-    if(n == 0 || m == 0) { 
-        return 0;
-    }
+// LCS function 
+int LCS(string &x, string &y, int n, int m, vv &t) { 
+    // BC
+    if(n == 0 || m == 0) return 0; 
 
-    // Present in t?
-    if(t[n][m] != -1) {
-        return t[n][m];
-    }
-    
-    // Choice diagram 
-    if(x[n-1] == y[m-1]) { 
-        return t[n][m] = 1 + LCS(x, y, n-1, m-1);
-    } else {
+    // Preset -> t?
+    if(t[n][m] != -1) return t[n][m];
+
+    // Choice Diagram 
+    // Chars matches
+    if(x[n-1] == y[m-1]) {
+        return t[n][m] = 1 + LCS(x, y, n - 1, m - 1, t);
+    } else { 
         return t[n][m] = max(
-            LCS(x, y, n, m-1),
-            LCS(x, y, n-1, m)
+            LCS(x, y, n, m - 1, t),
+            LCS(x, y, n - 1, m, t)
         );
     }
 }
+
 
 int main(){
     string x = "abcdgh";
@@ -42,8 +71,9 @@ int main(){
     int n = x.size();
     int m = y.size();
 
-    t.resize(n + 1, vector<int>(m + 1, -1)); // Initialization with -1 
-
-    cout << LCS(x, y, n, m) << endl; // op: 4 (abdh)
+    // DP table 
+    vv t(n + 1, v(m + 1, -1)); // Initially, INIT with -1
+    
+    cout << LCS(x, y, n, m, t) << endl; // op: 4 (abdh)
     return 0;
 }
