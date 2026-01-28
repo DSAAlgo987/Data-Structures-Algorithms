b37a53552c69fd1d822120f2e9ab864af49b1ad4
Faisal Khan <faisal@example.com>
2026-01-29 02:26:15 +0530

    completed mx of all subarray of size k

--- Algorithms/Sliding Window/Fixed_Size/Max_subarray_k/optimal.cpp
+++ Algorithms/Sliding Window/Fixed_Size/Max_subarray_k/optimal.cpp
@@ -1,43 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Time complexity: O(n)
-// Optimal soln
+/**
+ * PS: Maximum of all sub arrays of size k
+ * 
+ * PROBLEM: You are given an array/vector and size of the window. 
+ * Your task is to find out all the maximum number of all sub array of size k.
+ * 
+ * IP: vector<int> A, int k; 
+ * OP: vector<int> -> containing max elments of subarray of size k 
+ *
+ * APPROACHES: 
+ *  - Sliding window 
+ *      TC: O(n) 
+ *      SC: O(n)
+ * 
+ * LEARNING: 
+ *  - For optimization Queue(list) is best data structure along with sliding window 
+ */
 
-void solve(){
-    vector<int> arr = {1, 3, -1, -3, 5, 3, 6, 7};
-    int k = 3;
-    
-    int i = 0, j = 0;
+// aliases
+using v = vector<int>; 
 
-    list<int> l; // Dequeue 
+// display vector 
+void displayV(v A) { 
+    cout << "[ ";
+    for(auto x: A) 
+        cout << x << " ";
+    cout << "]";
+}
 
-    while(j < arr.size()){
-        // Calc 
-        while(l.size() > 0 && l.back() < arr[j]){
-            l.pop_back();
-        }
-        l.push_back(arr[j]);
+// sliding window 
+v mxSubarray(v &A, int k) { 
+    v ans; 
+    list<int> q; 
 
-        // Achieve window size 
-        if(j - i + 1 < k ){
-            j++;
-        }
-        else if(j - i + 1 == k){
-            // Ans <- calc 
-            cout << l.front() << " ";
+    int i = 0, j = 0; 
+    int n = A.size(); 
 
-            // Slide window 
-            if(arr[i] == l.front()){
-                l.pop_front();
-            }
+    while(j < n) { 
+        // calc 
+        while(q.size() > 0 && q.back() < A[j]) 
+            q.pop_back(); 
+
+        q.push_back(A[j]);
+
+        // achieve window size 
+        if(j - i + 1 < k) j++;
+
+        // hit window size 
+        else if(j - i + 1 == k) { 
+            // ans <- calc 
+            ans.push_back(q.front());
+
+            // slide window 
+            if(A[i] == q.front()) q.pop_front(); 
+
             i++;
             j++;
         }
+        
     }
+
+    // return the ans 
+    return ans; 
 }
 
 int main(){
-    solve();
+    v A = {1, 3, 2, -1, 1, 4, 3};
+    int k = 3;
+
+    displayV(mxSubarray(A, k));
+    
     return 0;
 }
